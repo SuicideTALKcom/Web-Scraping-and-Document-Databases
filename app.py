714db0615684b674014203789ba45708de100041
SuicideTALKcom <rice@dfnworld.com>
2019-05-07 07:13:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
-from flask import Flask, render_template, redirect
+# Import Dependencies 
+from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
 import scrape_mars
 import os
 
+# Create an instance of Flask app
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/app")
+# Use flask_pymongo to set up mongo connection locally 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
+# Create route that renders index.html template and finds documents from mongo
 @app.route("/")
-def index():
+def home(): 
+
+    # Find data
     mars = mongo.db.mars.find_one()
+
+    # Return template and data
     return render_template("index.html", mars=mars)
 
-@app.route('/scrape')
-def get():
+# Route that will trigger scrape function
+@app.route("/scrape")
+def scrape(): 
+
+    # Run scrapped functions
     mars = mongo.db.mars
-    marsdata = scrape_mars.scrape()
-    mars.update({}, marsdata, upsert=True)
+    mars_data = scrape_mars.scrape_mars_news()
+    mars_data = scrape_mars.scrape_mars_image()
+    mars_data = scrape_mars.scrape_mars_facts()
+    mars_data = scrape_mars.scrape_mars_weather()
+    mars_data = scrape_mars.scrape_mars_hemispheres()
+    mars.update({}, mars_data, upsert=True)
+
     return redirect("/", code=302)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == "__main__": 
+    app.run(debug= True)
